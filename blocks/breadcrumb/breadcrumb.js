f09a6e75138449e5f6ab55b04ab44be0a1cdbfbb
justinli-ensemble <69668698+justinli-ensemble@users.noreply.github.com>
2023-09-20 11:32:43 -0700

    adjusted how last item in breadcrumb is created

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,3 +1,4 @@
+import { getMetadata } from "../../scripts/lib-franklin.js";
 import { createTag } from "../../scripts/scripts.js";
 
 function createBreadcrumbItem(href, label) {
@@ -12,19 +13,26 @@ export default function decorate(block) {
     // If path ends with '/', pop the last item out. Then pop the current page
     const pathArr = window.location.pathname.split('/');
     if (pathArr[pathArr.length-1] == '') pathArr.pop();
+    const lastPath = pathArr[pathArr.length-1]; 
     pathArr.pop();
-
-    const currPageTitle = document.querySelector('h1');
     
+    // Formulate the list for breadcrumb
     const list = createTag('ul', { class: 'breadcrumb-list' });
     let segments = '/';
     pathArr.forEach((path) => {
         if (path !== '') segments += `${path}/`;
-
         list.append(createBreadcrumbItem(segments, path === '' ? 'HOME' : ` ▸ ${path.replaceAll('-', ' ')}`));
     })
 
-    list.append(` ▸ ${currPageTitle.innerText}`);
+    // Last item in breadcrumb should be current page title for articles and path for anything else
+    const templateType = getMetadata('template');
+    if (templateType == 'article') {
+        const currPageTitle = document.querySelector('h1');
+        list.append(` ▸ ${currPageTitle.innerText}`);
+    } else {
+        list.append(` ▸ ${lastPath.replaceAll('-', ' ')}`);
+    }
+    
 
     block.append(list);
 }
