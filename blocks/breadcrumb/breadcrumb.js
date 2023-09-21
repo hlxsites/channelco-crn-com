0dcf2619464bd3d86b6547f30c2862240491d6da
justinli-ensemble <69668698+justinli-ensemble@users.noreply.github.com>
2023-09-20 19:27:24 -0700

    Update breadcrumb.js

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,28 +1,28 @@
-import { createTag } from "../../scripts/scripts.js";
+import { createTag } from '../../scripts/scripts.js';
 
 function createBreadcrumbItem(href, label) {
-    return createTag('a', { href, class: 'breadcrumb-item' }, label);
+  return createTag('a', { href, class: 'breadcrumb-item' }, label);
 }
 
 export default function decorate(block) {
-    // If path ends with '/', pop the last item out. Then pop the current page
-    const pathArr = window.location.pathname.split('/');
-    if (pathArr[pathArr.length-1] == '') pathArr.pop();
-    const lastPath = pathArr[pathArr.length-1]; 
-    pathArr.pop();
-    
-    // Formulate the list for breadcrumb
-    const list = createTag('ul', { class: 'breadcrumb-list' });
-    let segments = '/';
-    pathArr.forEach((path) => {
-        if (path !== '') segments += `${path}/`;
-        list.append(createBreadcrumbItem(segments, path === '' ? 'HOME' : ` ▸ ${path.replaceAll('-', ' ')}`));
-    })
+  // If path ends with '/', pop the last item out. Then pop the current page
+  const pathArr = window.location.pathname.split('/');
+  if (pathArr[pathArr.length - 1] === '') pathArr.pop();
+  const lastPath = pathArr[pathArr.length - 1];
+  pathArr.pop();
 
-    // Last item in breadcrumb should be current page title. If not found, default to path
-    const currPageTitle = document.querySelector('h1');
-    list.append(` ▸ `);
-    list.append(currPageTitle ? currPageTitle.innerText : lastPath.replaceAll('-', ' '));
+  // Formulate the list for breadcrumb
+  const list = createTag('ul', { class: 'breadcrumb-list' });
+  let segments = '/';
+  pathArr.forEach((path) => {
+    if (path !== '') segments += `${path}/`;
+    list.append(createBreadcrumbItem(segments, path === '' ? 'HOME' : ` ▸ ${path.replaceAll('-', ' ')}`));
+  });
 
-    block.append(list);
+  // Last item in breadcrumb should be current page title. If not found, default to path
+  const currPageTitle = document.querySelector('h1');
+  list.append(' ▸ ');
+  list.append(currPageTitle ? currPageTitle.innerText : lastPath.replaceAll('-', ' '));
+
+  block.append(list);
 }
