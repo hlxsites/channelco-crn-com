3b41a27737e6e585d5b56fb8d8a4181013d38c3b
samuelopra <samuelopra@gmail.com>
2023-09-22 12:48:24 -0400

    use helper function

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,30 +1,27 @@
 // eslint-disable-next-line import/no-cycle
 import {
   sampleRUM,
-  loadBlocks,
 } from './lib-franklin.js';
 
-import { decorateMain } from './shared.js';
+import fetchFragment from './scripts.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
 
-async function loadAdFragment() {
+async function loadRightAdFragment() {
   const adFragmentContainer = document.getElementById('right-ad-fragment-container');
-  if (adFragmentContainer) {
-    const path = '/fragments/global-layout-right-fragment'; // Updated the fragment path
-    const resp = await fetch(`${path}.plain.html`);
-    if (resp.ok) {
-      const fragmentHTML = await resp.text();
-      adFragmentContainer.innerHTML = fragmentHTML;
-      decorateMain(adFragmentContainer);
-      await loadBlocks(adFragmentContainer);
-    } else {
-      adFragmentContainer.innerHTML = '<p>Error loading content</p>';
-    }
+  if (!adFragmentContainer) return;
+
+  try {
+    const fragmentHTML = await fetchFragment('/fragments/global-layout-right-fragment');
+    adFragmentContainer.innerHTML = '';
+    adFragmentContainer.appendChild(fragmentHTML);
+  } catch (error) {
+    console.error('Error fetching the fragment:', error);
+    adFragmentContainer.innerHTML = '<p>Error loading content</p>';
   }
 }
 
-await loadAdFragment();
+await loadRightAdFragment();
