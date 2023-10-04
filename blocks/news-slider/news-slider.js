74884e2a3e7e582b9efd294ab457dd708ef8fd47
samuelopra <samuelopra@gmail.com>
2023-10-04 11:48:03 -0400

    issue-82 remove console log

--- blocks/news-slider/news-slider.js
+++ blocks/news-slider/news-slider.js
@@ -110,7 +110,6 @@ function addDragEvents(handle, carousel, isTabsBlock) {
 }
 
 export default function decorate(block) {
-  console.log('block', block);
   const isTabsBlock = block.classList.contains('tabbed');
   let newsItems;
   let newsSlider;
