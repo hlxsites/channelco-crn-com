81de0a6f4a8897438d03f7701919805d90fa052c
vmitra <vmitra@adobe.com>
2023-10-10 13:23:22 -0400

    Update import.js

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -12,25 +12,24 @@
 /* global WebImporter */
 /* eslint-disable no-console, class-methods-use-this */
 const captializeFirstLetter = (str) => {
-const arr = str.split(" ");
-for (let i = 0; i < arr.length; i++) {
+  const arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-
-}
-const str2 = arr.join(" ");
-return str2;
-}
+  }
+  const str2 = arr.join(" ");
+  return str2;
+};
 
 const createMetadata = (main, document) => {
   const meta = {};
 
-  const title = document.querySelector('title');
+  const title = document.querySelector("title");
   if (title) {
-    meta.Title = title.textContent.replace(/[\n\t]/gm, '');
+    meta.Title = title.textContent.replace(/[\n\t]/gm, "");
   }
 
   const templateType = document.querySelector('[name="templateType"]');
-  if(templateType){
+  if (templateType) {
     meta.Template = templateType.content;
   }
   const desc = document.querySelector('[property="og:description"]');
@@ -39,24 +38,29 @@ const createMetadata = (main, document) => {
   }
 
   const authorName = document.querySelector('[name="author"]');
-  if(authorName) {
+  if (authorName) {
     meta.Author = authorName.content;
   }
 
-  const category = document.querySelector('.home');
-  if(category != null) {
+  const category = document.querySelector(".home");
+  if (category != null) {
     const categoryValue = captializeFirstLetter(category.textContent);
-    if ('Channel News' != categoryValue.trim() && categoryValue.includes('News')) {
-      meta.category = categoryValue.replace('News' , '');
+    if (
+      "Channel News" != categoryValue.trim() &&
+      categoryValue.includes("News")
+    ) {
+      meta.category = categoryValue.replace("News", "");
     } else {
-    meta.category = categoryValue;
+      meta.category = categoryValue;
     }
     category.parentNode.remove();
   }
 
-  const pubishDate = document.querySelector('[property="article:published_time"]');
+  const pubishDate = document.querySelector(
+    '[property="article:published_time"]'
+  );
   if (pubishDate) {
-    meta.PublishedDate = pubishDate.content;    
+    meta.PublishedDate = pubishDate.content;
   }
 
   const keywords = document.querySelector('[name="keywords"]');
@@ -64,28 +68,26 @@ const createMetadata = (main, document) => {
     meta.keywords = keywords.content;
   }
 
-  const pagination = document.querySelector('.pagination');
-  if(pagination) {
-    meta.SlideShow = 'true';
+  const pagination = document.querySelector(".pagination");
+  if (pagination) {
+    meta.SlideShow = "true";
   } else {
-    meta.SlideShow = 'false';
+    meta.SlideShow = "false";
   }
 
   const CompanyNames = document.querySelector('[name="CompanyNames"]');
-  if(CompanyNames) {
-    meta.companynames = CompanyNames.content; 
+  if (CompanyNames) {
+    meta.companynames = CompanyNames.content;
   }
 
   const CompanyWebpages = document.querySelector('[name="CompanyWebpages"]');
-  if(CompanyNames) {
-    meta.companywebpages = CompanyWebpages.content; 
+  if (CompanyNames) {
+    meta.companywebpages = CompanyWebpages.content;
   }
-
 
   const img = document.querySelector('[property="og:image"]');
   if (img && img.content) {
-    
-    const el = document.createElement('img');
+    const el = document.createElement("img");
     el.src = img.content;
     meta.Image = el;
   }
@@ -96,48 +98,46 @@ const createMetadata = (main, document) => {
   return meta;
 };
 
-
-
 const createFetchMetadata = (main, document) => {
   const meta = {};
 
-  const title = document.querySelector('.page-title');
+  const title = document.querySelector(".page-title");
   if (title) {
     meta.Title = title.textContent;
   }
 
   const templateType = document.querySelector('[name="templateType"]');
-  if(templateType){
+  if (templateType) {
     meta.Template = templateType.content;
   }
   const desc = document.querySelector('[property="og:description"]');
   if (desc) {
     meta.Description = desc.content;
   }
-  if (templateType.content === 'webpage'){
-  const category = document.querySelector('.page-title');
-  if(category != null) {
-    const categoryValue = captializeFirstLetter(category.textContent);   
-    meta.category = categoryValue;
+  if (templateType.content === "webpage") {
+    const category = document.querySelector(".page-title");
+    if (category != null) {
+      const categoryValue = captializeFirstLetter(category.textContent);
+      meta.category = categoryValue;
+    }
+    meta.Template = "Category";
   }
-  meta.Template = 'Category';
-  }
-  
+
   const CompanyNames = document.querySelector('[name="CompanyNames"]');
-  if(CompanyNames) {
-    meta.companynames = CompanyNames.content; 
+  if (CompanyNames) {
+    meta.companynames = CompanyNames.content;
   }
 
   const CompanyWebpages = document.querySelector('[name="CompanyWebpages"]');
-  if(CompanyNames) {
-    meta.companynames = CompanyWebpages.content; 
+  if (CompanyNames) {
+    meta.companynames = CompanyWebpages.content;
   }
 
   const keywords = document.querySelector('[name="keywords"]');
   if (keywords) {
     meta.Keywords = keywords.content;
   }
-  
+
   const block = WebImporter.Blocks.getMetadataBlock(document, meta);
   main.append(block);
 
@@ -146,46 +146,41 @@ const createFetchMetadata = (main, document) => {
 
 /** create Columns Banner block */
 const createBreadCrumbs = (main, document) => {
-  const selector = '.breadcrumb';
-  main.querySelectorAll(selector).forEach((breadcrumb) => {    
-    const bannerContent = '';    
+  const selector = ".breadcrumb";
+  main.querySelectorAll(selector).forEach((breadcrumb) => {
+    const bannerContent = "";
 
-    const cells = [
-      ['breadcrumb'],
-      [bannerContent],
-    ];
+    const cells = [["breadcrumb"], [bannerContent]];
     const table = WebImporter.DOMUtils.createTable(cells, document);
-    breadcrumb.innerHTML = '';
+    breadcrumb.innerHTML = "";
     breadcrumb.append(table);
   });
 };
 
 /** create Columns features block */
 const pagination = (main, document) => {
-  const selector = '.pagination';
-  main.querySelectorAll(selector).forEach((pagNav) => {   
-    const cells = [
-      ['pagination'],
-      [''],
-    ];
+  const selector = ".pagination";
+  main.querySelectorAll(selector).forEach((pagNav) => {
+    const cells = [["pagination"], [""]];
     const table = WebImporter.DOMUtils.createTable(cells, document);
     pagNav.append(table);
   });
 };
 
-
-
 /** create AuthorBio block */
 const createAuthorBio = (main, document) => {
-  main.querySelectorAll('.card.mb-3').forEach((authorbio) => {    
-    if(authorbio.querySelector('.author-bio-blurb')) {      
+  main.querySelectorAll(".card.mb-3").forEach((authorbio) => {
+    if (authorbio.querySelector(".author-bio-blurb")) {
       authorbio.remove();
-    } else if (authorbio.querySelector('.author-name-md')){
+    } else if (authorbio.querySelector(".author-name-md")) {
       authorbio.remove();
     }
   });
-  const authorNameMD = main.querySelector('.author-name-md');
-  if(null != authorNameMD.parentNode.id && 'news-article' === authorNameMD.parentNode.id) {
+  const authorNameMD = main.querySelector(".author-name-md");
+  if (
+    null != authorNameMD.parentNode.id &&
+    "news-article" === authorNameMD.parentNode.id
+  ) {
     authorNameMD.remove();
   } else {
     authorNameMD.parentNode.remove();
@@ -193,23 +188,18 @@ const createAuthorBio = (main, document) => {
 };
 
 const createAdBlock = (adBlock, main, document) => {
- 
-  adBlock.querySelectorAll('.int-ads').forEach((ad) => {
-    const div = ad.querySelector(':scope > div');    
-    const cells = [
-      ['AD'],
-      ['id', div.id],
-      ['type', 'Sponsored post'],
-    ];
+  adBlock.querySelectorAll(".int-ads").forEach((ad) => {
+    const div = ad.querySelector(":scope > div");
+    const cells = [["AD"], ["id", div.id], ["type", "Sponsored post"]];
     const table = WebImporter.DOMUtils.createTable(cells, document);
-    ad.parentNode.innerHTML = '';
-    let article = main.querySelector('.article');
-    if(!article) {
-      article = document.getElementById('news-article');      
+    ad.parentNode.innerHTML = "";
+    let article = main.querySelector(".article");
+    if (!article) {
+      article = document.getElementById("news-article");
     }
-    const p = article.querySelector('p:nth-of-type(5)');
+    const p = article.querySelector("p:nth-of-type(5)");
     if (p) {
-      p.after(table); 
+      p.after(table);
     } else {
       adBlock.replaceWith(table);
     }
@@ -217,88 +207,113 @@ const createAdBlock = (adBlock, main, document) => {
 };
 
 const removeLearnMore = (main, document) => {
-  main.querySelectorAll('.card').forEach((card) => {
-    const learnMore = card.querySelector('.learn-more-md');
-    if(learnMore) {
+  main.querySelectorAll(".card").forEach((card) => {
+    const learnMore = card.querySelector(".learn-more-md");
+    if (learnMore) {
       card.remove();
     }
   });
 };
 
+const insertBrightCoveBlock = (main, document) => {
+  main.querySelectorAll('.video-player').forEach((videoPlayer) => {
+    const videoJS = videoPlayer.getElementsByTagName('video-js')[0];
+    const playListID = videoJS.getAttribute('data-playlist-id');
+    const videoId = videoJS.getAttribute('data-video-id');
+    const player = videoJS.getAttribute('data-player');    
+    const cells = [["Brightcove"]];
+    if(player) {
+      cells.push(["player" , player]);
+    }
+    if (playListID) {
+      cells.push(["playlist", playListID]);
+    }
+    if (videoId) {
+      cells.push(["video" , videoId]);
+    }    
+    const table = WebImporter.DOMUtils.createTable(cells, document);
+    main.append(table);
+
+  });
+};
+
 export default {
   /**
-     * Apply DOM operations to the provided document and return
-     * the root element to be then transformed to Markdown.
-     * @param {HTMLDocument} document The document
-     * @param {string} url The url of the page imported
-     * @param {string} html The raw html (the document is cleaned up during preprocessing)
-     * @param {object} params Object containing some parameters given by the import process.
-     * @returns {HTMLElement} The root element to be transformed
-     */
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
   transformDOM: ({
     // eslint-disable-next-line no-unused-vars
-    document, url, html, params,
+    document,
+    url,
+    html,
+    params,
   }) => {
     // define the main element: the one that will be transformed to Markdown
     const main = document.body;
-    
+
     // use helper method to remove header, footer, etc.
     const templateType = document.querySelector('[name="templateType"]');
-    if(templateType.content === 'article') {
-    createAuthorBio(main, document);     
-    const adBlock = document.getElementById('imu1forarticles');
-    if(adBlock) {
-    createAdBlock(adBlock, main, document);
-    }
-    createMetadata(main, document);
-    removeLearnMore(main, document);
-    } else if(templateType.content === 'company'){
+    if (templateType.content === "article" || templateType.content === "slideshow") {
+      createAuthorBio(main, document);
+      const adBlock = document.getElementById("imu1forarticles");
+      if (adBlock) {
+        createAdBlock(adBlock, main, document);
+      }
+      createMetadata(main, document);
+      removeLearnMore(main, document);
+    } else if (templateType.content === "company") {      
       createFetchMetadata(main, document);
-      WebImporter.DOMUtils.remove(main, [
-        '.container.parent' 
-      ]);    
-    }
-    else if (templateType.content === 'webpage'){
+      WebImporter.DOMUtils.remove(main, [".container.parent"]);
+    } else if (templateType.content === "webpage") {
+      insertBrightCoveBlock(main, document);
       createFetchMetadata(main, document);
-      WebImporter.DOMUtils.remove(main, [
-        '.container.parent' 
-      ]);  
+      WebImporter.DOMUtils.remove(main, [".container.parent"]);
     }
     WebImporter.DOMUtils.remove(main, [
-      'nav',
-      'footer',
-      'time',
-      '.red-border-bottom', 
-      '.ad',
-      '.trending',
-      '.sponsored-resources',
-      '.rr-crn-awards',
-      '.ad-sponsored-post',
-      '.rr-crn-magazine',
-      '.breadcrumb',
-      '.modal',
-      '.ribbon',
-      '.back-to-top',
-      '.GLadv-728'    
-    ]);    
+      "nav",
+      "footer",
+      "time",
+      ".red-border-bottom",
+      ".ad",
+      ".trending",
+      ".sponsored-resources",
+      ".rr-crn-awards",
+      ".ad-sponsored-post",
+      ".rr-crn-magazine",
+      ".breadcrumb",
+      ".modal",
+      ".ribbon",
+      ".back-to-top",
+      ".GLadv-728",
+    ]);
     // create the metadata block and append it to the main element
-    
-   
 
     return main;
   },
 
   /**
-     * Return a path that describes the document being transformed (file name, nesting...).
-     * The path is then used to create the corresponding Word document.
-     * @param {HTMLDocument} document The document
-     * @param {string} url The url of the page imported
-     * @param {string} html The raw html (the document is cleaned up during preprocessing)
-     * @param {object} params Object containing some parameters given by the import process.
-     * @return {string} The path
-     */
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
   generateDocumentPath: ({
     // eslint-disable-next-line no-unused-vars
-    document, url, html, params,
-  }) => WebImporter.FileUtils.sanitizePath(new URL(url).pathname.replace(/\.htm$/, '').replace(/\/$/, '')),
+    document,
+    url,
+    html,
+    params,
+  }) =>
+    WebImporter.FileUtils.sanitizePath(
+      new URL(url).pathname.replace(/\.htm$/, "").replace(/\/$/, "")
+    ),
 };
