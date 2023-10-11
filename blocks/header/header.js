ab5ab464eb5b4acd0a6efd33e5c6ad06fb176a7f
Cole Walker <colew@ensemble.com>
2023-10-10 18:07:22 -0700

    close button in menu container

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -263,6 +263,21 @@ export default async function decorate(block) {
     const parentElement = main.parentNode;
     const menuEl = document.createElement('div');
     menuEl.classList.add('nav-container');
+
+    const closeBtn = document.createElement('div');
+    closeBtn.innerHTML = `
+          <button>Close
+            <span class="circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+            </span>
+          </button>
+          `;
+    closeBtn.classList.add('close-button');
+    closeBtn.querySelector('button').onclick = closeMenu;
+
+    menuContainer.prepend(closeBtn);
     parentElement.insertBefore(menuEl, main);
     menuEl.append(menuContainer);
   }
